a77c207f5546ca4bb4da95fd66cd9d099ba56e09
xixi <i@hexilee.me>
2022-09-12 15:20:56 +0800

    add cases: fast and slow tasks
    Signed-off-by: xixi <i@hexilee.me>

--- go/main.go
+++ go/main.go
@@ -2,11 +2,45 @@ package main
 
 import (
 	"net/http"
+	"os"
+
+	"github.com/google/uuid"
 )
 
 var data = make([]byte, 4096)
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func fastHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write(data)
+}
+
+func slowHandler(w http.ResponseWriter, r *http.Request) {
+	err := func() error {
+		filename := uuid.New().String()
+		file, err := os.Create(filename)
+		if err != nil {
+			return err
+		}
+
+		_, err = file.Write(data)
+		if err != nil {
+			return err
+		}
+		err = file.Sync()
+		if err != nil {
+			return err
+		}
+		err = file.Close()
+		if err != nil {
+			return err
+		}
+		return os.Remove(filename)
+	}()
+
+	if err != nil {
+		w.Write([]byte(err.Error()))
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.Write(data)
 }
 
@@ -14,6 +48,7 @@ func main() {
 	for i := range data {
 		data[i] = '0'
 	}
-	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/fast", fastHandler)
+	http.HandleFunc("/slow", slowHandler)
 	http.ListenAndServe(":8000", nil)
 }
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,22 +1,56 @@
 use std::convert::Infallible;
+use std::fs::{remove_file, File};
+use std::io::Write;
 use std::net::SocketAddr;
 
-use hyper::{Body, Request, Response, Server};
 use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server, StatusCode};
+use uuid::Uuid;
 
 const DATA: [u8; 4096] = ['0' as u8; 4096];
 
-async fn hello_world(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+async fn fast_handler(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
     Ok(Response::new(Body::from(DATA.as_slice())))
+}
+
+async fn slow_handler(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    if let Err(err) = tokio::task::spawn_blocking(slow_task)
+        .await
+        .expect("join err")
+    {
+        return Ok(Response::builder()
+            .status(StatusCode::INTERNAL_SERVER_ERROR)
+            .body(Body::from(err.to_string()))
+            .unwrap());
+    }
+    Ok(Response::new(Body::from(DATA.as_slice())))
+}
+
+fn slow_task() -> anyhow::Result<()> {
+    let filename = Uuid::new_v4().to_string();
+    let mut file = File::create(&filename)?;
+    file.write_all(DATA.as_slice())?;
+    file.sync_all()?;
+    drop(file);
+    remove_file(&filename)?;
+    Ok(())
 }
 
 #[tokio::main]
 async fn main() {
     let addr = SocketAddr::from(([0, 0, 0, 0], 8000));
 
-   
     let make_svc = make_service_fn(|_conn| async {
-        Ok::<_, Infallible>(service_fn(hello_world))
+        Ok::<_, Infallible>(service_fn(|req| async {
+            match req.uri().path() {
+                "/fast" => fast_handler(req).await,
+                "/slow" => slow_handler(req).await,
+                _ => Ok(Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(Body::empty())
+                    .unwrap()),
+            }
+        }))
     });
 
     let server = Server::bind(&addr).serve(make_svc);
