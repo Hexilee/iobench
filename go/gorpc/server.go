a5c0bdd56df891e5f6648b53ba5f8821a51c6545
xixi <i@hexilee.me>
2022-10-06 08:06:28 +0800

    use file pool in gorpc
    Signed-off-by: xixi <i@hexilee.me>

--- go/gorpc/server.go
+++ go/gorpc/server.go
@@ -4,9 +4,37 @@ import (
 	"bytes"
 	"io"
 	"os"
+	"sync"
 
 	"github.com/valyala/gorpc"
 )
+
+var filePool = sync.Pool{
+	New: func() any {
+		file, err := os.OpenFile("../data/data", os.O_RDONLY, 0)
+		if err != nil {
+			return err
+		}
+		return &File{file}
+	},
+}
+
+type File struct {
+	*os.File
+}
+
+func (f *File) Close() error {
+	_, err := f.Seek(0, 0)
+	if err != nil {
+		return err
+	}
+	filePool.Put(f)
+	return nil
+}
+
+func (f *File) WriteTo(w io.Writer) (int64, error) {
+	return io.Copy(w, f.File)
+}
 
 func ListenAndServe(addr string) error {
 	// Start rpc server serving registered service.
@@ -19,25 +47,28 @@ func ListenAndServe(addr string) error {
 			if request.Body != nil {
 				request.Body.Close()
 			}
-			file, err := os.OpenFile("../data/data", os.O_RDONLY, 0)
-			if err != nil {
-				buf := bytes.NewBufferString(err.Error())
+			switch ret := filePool.Get().(type) {
+			case error:
+				buf := bytes.NewBufferString(ret.Error())
 				return gorpc.Response{
 					Size: uint64(buf.Len()),
 					Body: io.NopCloser(buf),
 				}
-			}
-			stat, err := file.Stat()
-			if err != nil {
-				buf := bytes.NewBufferString(err.Error())
-				return gorpc.Response{
-					Size: uint64(buf.Len()),
-					Body: io.NopCloser(buf),
+			case *File:
+				stat, err := ret.Stat()
+				if err != nil {
+					buf := bytes.NewBufferString(err.Error())
+					return gorpc.Response{
+						Size: uint64(buf.Len()),
+						Body: io.NopCloser(buf),
+					}
 				}
-			}
-			return gorpc.Response{
-				Size: uint64(stat.Size()),
-				Body: file,
+				return gorpc.Response{
+					Size: uint64(stat.Size()),
+					Body: ret,
+				}
+			default:
+				panic("unreachable")
 			}
 		},
 	}
